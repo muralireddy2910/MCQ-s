cab00d6e10d96ff6b3682b5a136f0f19aee620df
muralireddy2910 <41356715+muralireddy2910@users.noreply.github.com>
2018-07-23 14:36:27 +0530

    Add files via upload

---
+++ home.js
@@ -0,0 +1,64 @@
+/*function show1(){
+  document.getElementById('div1').style.display ='none';
+}*/
+
+function show1(){
+  document.getElementById('div1').style.display = 'block';
+}
+function show2(){
+  document.getElementById('div2').style.display = 'block';
+}
+function show3(){
+  document.getElementById('div3').style.display = 'block';
+}
+function show4(){
+  document.getElementById('div4').style.display = 'block';
+}
+function show5(){
+  document.getElementById('div5').style.display = 'block';
+}
+function show6(){
+  document.getElementById('div6').style.display = 'block';
+}
+function show7(){
+  document.getElementById('div7').style.display = 'block';
+}
+function show8(){
+  document.getElementById('div8').style.display = 'block';
+}
+function show9(){
+  document.getElementById('div9').style.display = 'block';
+}
+function show10(){
+  document.getElementById('div10').style.display = 'block';
+}
+function show11(){
+  document.getElementById('div11').style.display = 'block';
+}
+function show12(){
+  document.getElementById('div12').style.display = 'block';
+}
+function show13(){
+  document.getElementById('div13').style.display = 'block';
+}
+function show14(){
+  document.getElementById('div14').style.display = 'block';
+}
+function show15(){
+  document.getElementById('div15').style.display = 'block';
+}
+function show16(){
+  document.getElementById('div16').style.display = 'block';
+}
+function show17(){
+  document.getElementById('div17').style.display = 'block';
+}
+function show18(){
+  document.getElementById('div18').style.display = 'block';
+}
+function show19(){
+  document.getElementById('div19').style.display = 'block';
+}
+function show20(){
+  document.getElementById('div20').style.display = 'block';
+}
